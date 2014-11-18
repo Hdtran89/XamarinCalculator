64cc78af2cd92c1ebb14c52b002ef37f9860050f
Hdtran89 <hdtran89@yahoo.com>
2014-11-18 01:52:46 -0600

    calculator

---
+++ XamarinCalculator/XamarinCalculatorViewController.designer.cs
@@ -0,0 +1,157 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace XamarinCalculator
+{
+	[Register ("XamarinCalculatorViewController")]
+	partial class XamarinCalculatorViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel Calculator { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Divided { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Eight { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Equal { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Five { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Four { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Multiplied { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Nine { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton One { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Plus { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel Screen { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Seven { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Six { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Subtracted { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Three { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Two { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Zero { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (Calculator != null) {
+				Calculator.Dispose ();
+				Calculator = null;
+			}
+			if (Divided != null) {
+				Divided.Dispose ();
+				Divided = null;
+			}
+			if (Eight != null) {
+				Eight.Dispose ();
+				Eight = null;
+			}
+			if (Equal != null) {
+				Equal.Dispose ();
+				Equal = null;
+			}
+			if (Five != null) {
+				Five.Dispose ();
+				Five = null;
+			}
+			if (Four != null) {
+				Four.Dispose ();
+				Four = null;
+			}
+			if (Multiplied != null) {
+				Multiplied.Dispose ();
+				Multiplied = null;
+			}
+			if (Nine != null) {
+				Nine.Dispose ();
+				Nine = null;
+			}
+			if (One != null) {
+				One.Dispose ();
+				One = null;
+			}
+			if (Plus != null) {
+				Plus.Dispose ();
+				Plus = null;
+			}
+			if (Screen != null) {
+				Screen.Dispose ();
+				Screen = null;
+			}
+			if (Seven != null) {
+				Seven.Dispose ();
+				Seven = null;
+			}
+			if (Six != null) {
+				Six.Dispose ();
+				Six = null;
+			}
+			if (Subtracted != null) {
+				Subtracted.Dispose ();
+				Subtracted = null;
+			}
+			if (Three != null) {
+				Three.Dispose ();
+				Three = null;
+			}
+			if (Two != null) {
+				Two.Dispose ();
+				Two = null;
+			}
+			if (Zero != null) {
+				Zero.Dispose ();
+				Zero = null;
+			}
+		}
+	}
+}
